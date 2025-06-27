3512e7eec496a8a2ad113e30b041b5c27ec6d7af
ChickenTender268 <pablocbb22@gmail.com>
2025-06-27 16:42:33 +0200

    Create contractABI.js

---
+++ server/contractABI.js
@@ -0,0 +1,30 @@
+const ABI = [
+  {
+    "inputs": [
+      { "internalType": "address", "name": "recipient", "type": "address" },
+      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+    ],
+    "name": "convertAndForward",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "withdrawFees",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      { "internalType": "address", "name": "", "type": "address" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+module.exports = ABI;
