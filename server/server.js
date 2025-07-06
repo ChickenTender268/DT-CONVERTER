670d25435aa826c4706f33bdb0c746dc670e7a66
ChickenTender268 <pablocbb22@gmail.com>
2025-07-06 13:48:33 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,17 +16,13 @@ const provider = new ethers.JsonRpcProvider(INFURA_URL);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
 
-
-app.use(cors()); {
-origin: 'https://baibaic.xyz'
-app.use(express.json())};
-
+app.use(cors({ origin: 'https://baibaic.xyz' }));
+app.use(express.json());
 
 app.post("/api/quote", async (req, res) => {
   const { fiatAmount, fiatCurrency, cryptoCurrency } = req.body;
-
   try {
-    const rate = 1; 
+    const rate = 1;
     const cryptoAmount = fiatAmount * rate;
     const chargePercentage = 0.088;
     const adjustedCryptoAmount = cryptoAmount * (1 - chargePercentage);
@@ -46,14 +42,9 @@ app.post("/api/quote", async (req, res) => {
 });
 
 app.post("/api/initiate", async (req, res) => {
-  const { userAddress, fiatAmount, fiatCurrency, cryptoCurrency } = req.body;
-
   try {
     await new Promise(resolve => setTimeout(resolve, 3000));
-
-  
     const paybisUrl = `https://paybis.com/?refId=42748`;
-
     res.status(200).json({ url: paybisUrl });
   } catch (err) {
     console.error("Initiate API Error:", err);
